422d28cb1aa34a15882665acb78c5e9a47246635
avroopa12 <134259780+avroopa12@users.noreply.github.com>
2026-03-30 15:46:56 +0530

    Add additional personal information variables

--- day2/variable.py
+++ day2/variable.py
@@ -1,7 +1,23 @@
 //day 2 : 30 days of python programming 
 first_name = " harry "
-last_name = " potter " 
+
+last_name = " potter "
+
 full_nme = "harry potter "
+
 print("First name :",first_name)
 print("last name :",last_name)
 print("full name :",full_name)
+
+county = "india"
+
+city = "knapur"
+
+age = 23
+
+print("County :",county)
+
+print("City :",city)
+
+print("Age :",age)
+
