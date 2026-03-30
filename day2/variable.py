3e57a5a68c5126e0a039ac7a980b9d7a94d92434
avroopa12 <134259780+avroopa12@users.noreply.github.com>
2026-03-30 15:59:14 +0530

    Update variable.py

--- day2/variable.py
+++ day2/variable.py
@@ -15,9 +15,32 @@ city = "knapur"
 
 age = 23
 
+year = 2026
+
 print("County :",county)
 
 print("City :",city)
 
 print("Age :",age)
+
+print("Year :",year)
+
+is_married = False
+
+is_true = True
+
+is_light_on = "no"
+
+print("Married :",is_marrige)
+
+print("Is_true :",is_true)
+
+print("Is_light_on :",is_light_on)
+
+first_name,last_name,full_name = "harry", "potter","harrry potter"
+
+print(first_name,last_name,full_name)
+
+
+
 
