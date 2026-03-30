b790f9d172b2d62dc28783fb25f4a4bf781a67da
avroopa12 <134259780+avroopa12@users.noreply.github.com>
2026-03-30 15:36:38 +0530

    Add first and last name variables with print statements
    Fix variable name typo and format output

--- day2/variable.py
+++ day2/variable.py
@@ -1,1 +1,7 @@
-
+//day 2 : 30 days of python programming 
+first_name = " harry "
+last_name = " potter " 
+full_nme = "harry potter "
+print("First name :",first_name)
+print("last name :",last_name)
+print("full name :",full_name)
